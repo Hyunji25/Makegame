82f5828e2cbbd8390dfb939228d44dba6e6c6eb5
admin <>
2023-03-15 13:44:29 +0900

    Makegame

--- MakeGameUnity/Assets/Scripts/EnemyManager.cs
+++ MakeGameUnity/Assets/Scripts/EnemyManager.cs
@@ -17,6 +17,10 @@ public class EnemyManager : MonoBehaviour
         }
     }
 
+    // 생성되는 Enemy를 담아둘 상위 객체
+    private GameObject Parent;
+
+    // Enemy로 사용할 원형 객체
     private GameObject Prefab;
 
     private void Awake()
@@ -28,19 +32,36 @@ public class EnemyManager : MonoBehaviour
             // 씬이 변경되어도 계속 유지될 수 있게 해준다
             DontDestroyOnLoad(gameObject);
 
+            // 생성되는 Enemy를 담아둘 상위 객체
+            Parent = new GameObject("EnemyList");
+
+            // Enemy로 사용할 원형 객체
             Prefab = Resources.Load("Prefabs/Enemy/Enemy") as GameObject;
         }
     }
 
+    // 시작하자마자 Start 함수를 코루틴 함수로 실행
     private IEnumerator Start()
     {
         while(true)
         {
+            // Enemy 원형 객체를 복제한다
             GameObject Obj = Instantiate(Prefab);
 
-            Obj.transform.position = new Vector3(18.0f, Random.Range(-8.5f, -5.5f), 0.0f);
-            Obj.transform.name = "TEST";
+            // Enemy 작동 스크립트 포함
+            //Obj.AddComponent<EnemyController>();
 
+            // 클론의 위치를 초기화
+            Obj.transform.position = new Vector3(18.0f, Random.Range(-8.5f, -6.5f), 0.0f);
+            //Obj.transform.position = new Vector3(18.0f, -7.5f, 0.0f);
+
+            // 클론의 이름 초기화
+            Obj.transform.name = "Enemy";
+
+            // 클론의 계층구조 설정
+            Obj.transform.parent = Parent.transform;
+
+            // 1.5초 휴식
             yield return new WaitForSeconds(1.5f);
         }
     }
--- MakeGameUnity/Assets/Scripts/PlayerController.cs
+++ MakeGameUnity/Assets/Scripts/PlayerController.cs
@@ -1,115 +1,111 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
+
+
 
 public class PlayerController : MonoBehaviour
 {
-    // 움직이는 속도
+    // ** 움직이는 속도
     private float Speed;
 
-    // 움직임을 저장하는 벡터
+    // ** 움직임을 저장하는 벡터
     private Vector3 Movement;
 
-    // 플레이어의 Animator 구성요소를 받아오기 위해...
+    // ** 플레이어의 Animator 구성요소를 받아오기위해...
     private Animator animator;
 
-    // 플레이어의 SpriteRenderer 구성요소를 받아오기 위해...
+    // ** 플레이어의 SpriteRenderer 구성요소를 받아오기위해...
     private SpriteRenderer playerRenderer;
 
-    // 상태 체크
-    private bool onAttack; // 공격 상태
-    private bool onHit; // 피격 상태
-    private bool onRolling;
-    private bool onDeath;
-    private bool onAlive;
-    private bool onClimbing;
+    // ** [상태체크]
+    private bool onAttack; // 공격상태
+    private bool onHit; // 피격상태
 
-    // 복사할 총알 원본
+    // ** 복제할 총알 원본
     private GameObject BulletPrefab;
 
-    // 복제할 FX 원본
+    // ** 복제할 FX 원본
     private GameObject fxPrefab;
 
     // 추후 list로 변경
     public GameObject[] stageBack = new GameObject[7];
 
-    // 복제된 총알의 저장공간
+    /*
+    Dictionary<string, object>;
+    Dictionary<string, GameObject>;
+     */
+
+    // ** 복제된 총알의 저장공간.
     private List<GameObject> Bullets = new List<GameObject>();
 
-    // 플레이어가 마지막으로 바라본 방향
+    // ** 플레이어가 마지막으로 바라본 방향.
     private float Direction;
 
-    // 플레이어가 바라보는 방향
+    [Header("방향")]
+    // ** 플레이어가 바라보는 방향
+
+    [Tooltip("왼쪽")]
     public bool DirLeft;
+    [Tooltip("오른쪽")]
     public bool DirRight;
 
-    //private ControllerManager Manager = new ControllerManager();
 
     private void Awake()
     {
-        // Player의 Animator를 받아온다
+        // ** player 의 Animator를 받아온다.
         animator = this.GetComponent<Animator>();
 
-        // Player의 SpriteRenderer를 받아온다
+        // ** player 의 SpriteRenderer를 받아온다.
         playerRenderer = this.GetComponent<SpriteRenderer>();
 
-        // [Resources] 폴더에서 사용할 리소스를 들고온다
+        // ** [Resources] 폴더에서 사용할 리소스를 들고온다.
         BulletPrefab = Resources.Load("Prefabs/Bullet") as GameObject;
-        fxPrefab = Resources.Load("Prefabs/FX/Smoke") as GameObject;
+        fxPrefab = Resources.Load("Prefabs/FX/Hit") as GameObject;
     }
 
-    // 유니티 기본 제공 함수
-    // 초기값을 설정할 때 사용
+    // ** 유니티 기본 제공 함수
+    // ** 초기값을 설정할 때 사용
     void Start()
     {
-        // 속도를 초기화
+        // ** 속도를 초기화.
         Speed = 5.0f;
 
-        // 초기값 세팅
+        // ** 초기값 셋팅
         onAttack = false;
         onHit = false;
-        onRolling = false;
-        onDeath = false;
-        onAlive = false;
-        onClimbing = false;
         Direction = 1.0f;
 
         DirLeft = false;
         DirRight = false;
 
-        for (int i = 0; i < 7; i++)
-        {
+        for (int i = 0; i < 7; ++i)
             stageBack[i] = GameObject.Find(i.ToString());
-        }
     }
 
-    // 유니티 기본 제공 함수
-    // 프레임마다 반복적으로 실행되는 함수
+    // ** 유니티 기본 제공 함수
+    // ** 프레임마다 반복적으로 실행되는 함수.
     void Update()
     {
-        // 실수 연산 IEEE754
+        // **  Input.GetAxis =     -1 ~ 1 사이의 값을 반환함. 
+        float Hor = Input.GetAxisRaw("Horizontal"); // -1 or 0 or 1 셋중에 하나를 반환.
+        float Ver = Input.GetAxisRaw("Vertical"); // -1 or 0 or 1 셋중에 하나를 반환.
 
-        // Input.GetAxis =     -1 ~ 1 사이의 값을 실수로 반환
-        // Input.GetAxisRaw =     -1 or 0 or 1 반환
-        float Hor = Input.GetAxisRaw("Horizontal");
-        float Ver = Input.GetAxisRaw("Vertical");
-
-        // 입력 받은 값으로 플레이어를 움직인다
+        // ** 입력받은 값으로 플레이어를 움직인다.
         Movement = new Vector3(
-        Hor * Time.deltaTime * Speed,
-        Ver * Time.deltaTime * (Speed * 0.5f),
-        0.0f);
+            Hor * Time.deltaTime * Speed,
+            Ver * Time.deltaTime * (Speed * 0.5f),
+            0.0f);
 
         transform.position += new Vector3(0.0f, Movement.y, 0.0f);
 
-        // Hor이 0이라면 멈춰있는 상태이므로 예외처리를 해준다
+        // ** Hor이 0이라면 멈춰있는 상태이므로 예외처리를 해준다. 
         if (Hor != 0)
             Direction = Hor;
 
-        if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
-        {  
-            // 플레이어의 좌표가 0.1 보다 작을 때 플레이어만 움직인다
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            // ** 플레이어의 좌표가 0.1f 보다 작을때 플레이어만 움직인다.
             if (transform.position.x < 0.1f)
                 transform.position += Movement;
             else
@@ -124,187 +120,120 @@ public class PlayerController : MonoBehaviour
             ControllerManager.GetInstance().DirRight = false;
             ControllerManager.GetInstance().DirLeft = true;
 
-            // 플레이어의 좌표가 -15.0 보다 클 때 플레이어만 움직인다
+            // ** 플레이어의 좌표가 -15.0 보다 클때 플레이어만 움직인다.
             if (transform.position.x > -15.0f)
-            // 실제 플레이어를 움직인다
+                // ** 실제 플레이어를 움직인다.
                 transform.position += Movement;
         }
 
-        if (Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.LeftArrow))
+
+
+        if (Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.LeftArrow) ||
+            Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
         {
             ControllerManager.GetInstance().DirRight = false;
             ControllerManager.GetInstance().DirLeft = false;
         }
 
-        // 플레이어가 바라보고 있는 방향에 따라 이미지 반전 설정
+
+        // ** 플레이어가 바라보고있는 방향에 따라 이미지 반전 설정.
         if (Direction < 0)
         {
             playerRenderer.flipX = DirLeft = true;
-
-            // 실제 플레이어를 움직인다
-            transform.position += Movement;
         }
-
         else if (Direction > 0)
         {
             playerRenderer.flipX = false;
             DirRight = true;
         }
 
-        // 스페이스바를 입력한다면...
+
+        // ** 좌측 쉬프트키를 입력한다면.....
+        if (Input.GetKey(KeyCode.LeftShift))
+            // ** 피격
+            OnHit();
+
+        // ** 스페이스바를 입력한다면..
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            OnAttack(); // 공격
+            // ** 공격
+            OnAttack();
 
-            // 총알 원본을 복제한다
+            // ** 총알원본을 본제한다.
             GameObject Obj = Instantiate(BulletPrefab);
 
-            // 복제된 총알의 위치를 현재 플레이어의 위치로 초기화한다
+            // ** 복제된 총알의 위치를 현재 플레이어의 위치로 초기화한다.
             Obj.transform.position = transform.position;
 
-            // 총알의 BulletController 스크립트를 받아온다
+            // ** 총알의 BullerController 스크립트를 받아온다.
             BulletController Controller = Obj.AddComponent<BulletController>();
 
-            // 총알 스크립트 내부의 방향 변수를 현재 플레이어의 방향 변수로 설정한다
+            // ** 총알 스크립트내부의 방향 변수를 현재 플레이어의 방향 변수로 설정 한다.
             Controller.Direction = new Vector3(Direction, 0.0f, 0.0f);
 
-            // 총알 스크립트 내부의 FX Prefab을 설정한다
+            // ** 총알 스크립트내부의 FX Prefab을 설정한다.
             Controller.fxPrefab = fxPrefab;
 
-            // 총알의 SpriteRenderer를 받아온다
-            SpriteRenderer bulletrenderer = Obj.GetComponent<SpriteRenderer>();
+            // ** 총알의 SpriteRenderer를 받아온다.
+            SpriteRenderer buleltRenderer = Obj.GetComponent<SpriteRenderer>();
 
-            // 총알의 이미지 반전 상태를 플레이어의 이미지 반전 상태로 설정한다
-            bulletrenderer.flipY = playerRenderer.flipX;
+            // ** 총알의 이미지 반전 상태를 플레이어의 이미지 반전 상태로 설정한다.
+            buleltRenderer.flipY = playerRenderer.flipX;
 
-            // 모든 설정이 종료되었다면 저장소에 보관한다
+            // ** 모든 설정이 종료되었다면 저장소에 보관한다.
             Bullets.Add(Obj);
         }
 
-        // 좌측 쉬프트키를 입력한다면...
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-            OnHit(); // 피격
-
-        if (Input.GetKeyDown(KeyCode.E))
-            OnRolling();
-
-        if (Input.GetKeyDown(KeyCode.R))
-            OnDeath();
-
-        if (Input.GetKeyDown(KeyCode.Q))
-            OnAlive();
-
-        if (Input.GetKeyDown(KeyCode.V))
-            OnClimbing();
-
-        // 플레이어의 움직임에 따라 이동 모션을 실행한다
+        // ** 플레이의 움직임에 따라 이동 모션을 실행 한다.
         animator.SetFloat("Speed", Hor);
-
-        // deltaTime : 1프레임과 2프레임 사이의 시간차
     }
 
     private void OnAttack()
     {
-        // 이미 공격 모션이 진행 중이라면
+        // ** 이미 공격모션이 진행중이라면
         if (onAttack)
-            // 함수를 종료시킨다
+            // ** 함수를 종료시킨다.
             return;
 
-        // 함수가 종료되지 않았다면 
-        // 공격 상태를 활성화하고
+        // ** 함수가 종료되지 않았다면...
+        // ** 공격상태를 활성화 하고.
         onAttack = true;
 
-        // 공격 모션을 실행시킨다
+        // ** 공격모션을 실행 시킨다.
         animator.SetTrigger("Attack");
     }
 
     private void SetAttack()
     {
-        // 함수가 실행되면 공격 모션이 비활성화된다
-        // 함수는 애니메이션 클립의 이벤트 프레임으로 삽입됨
+        // ** 함수가 실행되면 공격모션이 비활성화 된다.
+        // ** 함수는 애니매이션 클립의 이벤트 프레임으로 삽입됨.
         onAttack = false;
     }
 
     private void OnHit()
     {
-        // 이미 피격 모션이 진행 중이라면
+        // ** 이미 피격모션이 진행중이라면
         if (onHit)
-            // 함수를 종료시킨다
+            // ** 함수를 종료시킨다.
             return;
 
-        // 함수가 종료되지 않았다면 
-        // 피격 상태를 활성화하고
+        // ** 함수가 종료되지 않았다면...
+        // ** 피격상태를 활성화 하고.
         onHit = true;
 
-        // 피격 모션을 실행시킨다
+        // ** 피격모션을 실행 시킨다.
         animator.SetTrigger("Hit");
     }
 
     private void SetHit()
     {
-        // 함수가 실행되면 피격 모션이 비활성화된다
-        // 함수는 애니메이션 클립의 이벤트 프레임으로 삽입됨
+        // ** 함수가 실행되면 피격모션이 비활성화 된다.
+        // ** 함수는 애니매이션 클립의 이벤트 프레임으로 삽입됨.
         onHit = false;
     }
 
-    private void OnRolling()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (onRolling)
-            return;
 
-        onRolling = true;
-        animator.SetTrigger("Rolling");
-    }
-
-    private void SetRolling()
-    {
-        onRolling = false;
-    }
-
-    private void OnDeath()
-    {
-        if (onDeath)
-            return;
-
-        onDeath = true;
-        animator.SetTrigger("Death");
-    }
-
-    private void SetDeath()
-    {
-        onDeath = false;
-    }
-
-    private void OnAlive()
-    {
-        if (onAlive)
-            return;
-
-        onAlive = true;
-        animator.SetTrigger("Idol");
-    }
-
-    private void SetAlive()
-    {
-        onAlive = false;
-    }
-
-    private void OnClimbing()
-    {
-        if (onClimbing)
-            return;
-
-        onClimbing = true;
-        animator.SetTrigger("Climbing");
-    }
-
-    private void SetClimbing()
-    {
-        onClimbing = false;
-    }
-
-    private void OnCollisionEnter2D(Collider2D collision)
-    {
-        print("Call");
     }
 }
