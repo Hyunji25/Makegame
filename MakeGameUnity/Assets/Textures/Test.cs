afb6284eb8cb53ebd463c247c20a18e0a08459b0
admin <chj09255@gmail.com>
2023-03-29 13:51:39 +0900

    Makegame

--- MakeGameUnity/Assets/Textures/Test.cs
+++ MakeGameUnity/Assets/Textures/Test.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class HPTest : MonoBehaviour
 {
     public GameObject ui;
     public RectTransform uiTranspos;
